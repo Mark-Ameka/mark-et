f2861481404eb8f544fceb04a0b046aed5dbe080
Mark-Ameka <xenonmendozaxenon@gmail.com>
2023-11-06 22:04:13 +0800

    ui fixed for authenticated users

--- resources/views/market/index.blade.php
+++ resources/views/market/index.blade.php
@@ -104,18 +104,31 @@
                                 </div>
                                 
                             </div>
-                            <div class="grid grid-cols-2 gap-3 items-center justify-center mt-3">
-                                <div class="md:hidden block">
-                                    <a href="{{ route('user.show', $item->seller['id']) }}" class="flex justify-center text-white bg-sky-900 rounded-md py-2">
-                                        Check Profile
-                                    </a>
+                            @if ($item->seller['id'] == Auth::id())
+                                <div class="flex justify-end items-center py-2 mr-2">
+                                    <div class="block md:hidden text-yellow-300 font-bold">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-license" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                            <path d="M15 21h-9a3 3 0 0 1 -3 -3v-1h10v2a2 2 0 0 0 4 0v-14a2 2 0 1 1 2 2h-2m2 -4h-11a3 3 0 0 0 -3 3v11"></path>
+                                            <path d="M9 7l4 0"></path>
+                                            <path d="M9 11l4 0"></path>
+                                        </svg>
+                                    </div>
                                 </div>
-                                <div class="md:hidden block">
-                                    <a href="{{ route('item.show', $item->id) }}" class="flex justify-center text-white bg-green-900 rounded-md py-2">
-                                        Check Item
-                                    </a>
+                            @else
+                                <div class="grid grid-cols-2 gap-3 items-center justify-center mt-3">
+                                    <div class="md:hidden block">
+                                        <a href="{{ route('user.show', $item->seller['id']) }}" class="flex justify-center text-white bg-sky-900 rounded-md py-2">
+                                            Check Profile
+                                        </a>
+                                    </div>
+                                    <div class="md:hidden block">
+                                        <a href="{{ route('item.show', $item->id) }}" class="flex justify-center text-white bg-green-900 rounded-md py-2">
+                                            Check Item
+                                        </a>
+                                    </div>
                                 </div>
-                            </div>
+                            @endif
                             <div class="flex justify-center">
                                 {{-- View item --}}
                                 @if ($item->seller['id'] == Auth::id())
