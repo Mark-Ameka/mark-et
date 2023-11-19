329dc32d1ec01f60dab8fe7a8f60a63f42ee1688
Mark-Ameka <xenonmendozaxenon@gmail.com>
2023-11-19 19:45:17 +0800

    show password js

--- public/components.js
+++ public/components.js
@@ -71,3 +71,56 @@ document.addEventListener("DOMContentLoaded", function () {
             "rgb(" + dominantColor.join(",") + ")";
     }
 });
+
+// show password
+document.addEventListener("DOMContentLoaded", function () {
+    if (document.getElementById("toggle_pass") != null) {
+        document
+            .getElementById("toggle_pass")
+            .addEventListener("change", function () {
+                var passwordInput = document.getElementById("password");
+                var passwordConf = document.getElementById("password-confirm");
+                var svgContainer = document.getElementById("append-svg");
+
+                // Clear previous SVG content
+                svgContainer.innerHTML = "";
+
+                if (this.checked) {
+                    passwordInput.type = "text";
+                    if (passwordConf != null) {
+                        passwordConf.type = "text";
+                    }
+
+                    // Create a new div element to hold the SVG content
+                    var div = document.createElement("div");
+                    div.innerHTML = `
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-eye-closed absolute right-2 top-2 text-neutral-300" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                <path d="M21 9c-2.4 2.667 -5.4 4 -9 4c-3.6 0 -6.6 -1.333 -9 -4" />
+                                <path d="M3 15l2.5 -3.8" /><path d="M21 14.976l-2.492 -3.776" />
+                                <path d="M9 17l.5 -4" /><path d="M15 17l-.5 -4" />
+                            </svg>
+                        `;
+
+                    // Append the new div (containing the SVG) to the target element
+                    svgContainer.appendChild(div);
+                } else {
+                    passwordInput.type = "password";
+                    if (passwordConf != null) {
+                        passwordConf.type = "password";
+                    }
+                    var div = document.createElement("div");
+                    div.innerHTML = `
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-eye absolute right-2 top-2 text-neutral-300" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                <path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
+                                <path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" />
+                            </svg>
+                        `;
+
+                    // Append the new div (containing the SVG) to the target element
+                    svgContainer.appendChild(div);
+                }
+            });
+    }
+});
